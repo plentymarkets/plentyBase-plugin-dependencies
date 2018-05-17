2445c62011b782895848d0e64abd6bbe2cc42cf4
Jerome Klemm <jerome.klemm@plentymarkets.com>
2018-05-17 16:52:31 +0200

    Removed pathSegmentList parameter from EventBean

--- src/main/java/event/EventBean.java
+++ src/main/java/event/EventBean.java
@@ -1,15 +1,11 @@
 package event;
 
-import javax.ws.rs.core.PathSegment;
-import java.util.List;
-
 /**
  * @author deubel
  */
 public class EventBean
 {
 	private String identifier;
-	private List<PathSegment> pathSegmentList;
 	private String jsonData;
 
 	public String getIdentifier()
@@ -20,16 +16,6 @@ public class EventBean
 	public void setIdentifier(String identifier)
 	{
 		this.identifier = identifier;
-	}
-
-	public List<PathSegment> getPathSegmentList()
-	{
-		return pathSegmentList;
-	}
-
-	public void setPathSegmentList(List<PathSegment> pathSegmentList)
-	{
-		this.pathSegmentList = pathSegmentList;
 	}
 
 	public String getJsonData()
